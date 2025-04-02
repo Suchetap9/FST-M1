6dc77b395ff03cb8b8ec083f289f2004023c64b4
Suchetap9 <suchepat@in.ibm.com>
2025-04-02 16:20:40 +0530

    Update Activity6.py

--- Selenium/Activities/Activity6.py
+++ Selenium/Activities/Activity6.py
@@ -1,21 +1,22 @@
 # Import webdriver from selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
 
 # Start the Driver
 with webdriver.Firefox() as driver:
-    # Declare the wait variable
-    wait = WebDriverWait(driver, timeout=10)
     # Navigate to the URL
-    driver.get("https://training-support.net/webelements/dynamic-content")
+    driver.get("https://training-support.net/webelements/dynamic-controls")
+
     # Print the title of the page
     print("Page title is: ", driver.title)
 
-    # Find the button and click it
-    driver.find_element(By.ID, "genButton").click()
-    # Wait for the word to appear
-    if wait.until(EC.text_to_be_present_in_element((By.ID, "word"), "release")):
-        # Print the text to console
-        print("Word found: ", driver.find_element(By.ID, "word").text)
+	# Find the checkbox
+    checkbox = driver.find_element(By.ID, "checkbox")
+    # Click it
+    checkbox.click()
+    # Verify if the checkbox is selected or not
+    print("Checkbox is visible: ", checkbox.is_selected())
+    # Click it again
+    checkbox.click()
+    # Verify again if the checkbox is selected or not
+    print("Checkbox is visible: ", checkbox.is_selected())
