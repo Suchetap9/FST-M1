90dfb3fc002134baa2ff9ede5d2c351a9ac6827c
Suchetap9 <suchepat@in.ibm.com>
2025-04-02 16:14:42 +0530

    Update Activity6.java

--- Selenium/Activities/Activity6.java
+++ Selenium/Activities/Activity6.java
@@ -1,31 +1,31 @@
 package activities;
 
-import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class Activity12 {
+public class Activity6 {
     public static void main(String[] args) {
-        // Create a new instance of the Firefox driver
+        // Initialize the Firefox driver
         WebDriver driver = new FirefoxDriver();
-        // Create the Wait object
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
         // Open the page
-        driver.get("https://www.training-support.net/webelements/dynamic-content");
+        driver.get("https://training-support.net/webelements/dynamic-controls");
         // Print the title of the page
         System.out.println("Page title: " + driver.getTitle());
 
-        // Find the button and click it
-        driver.findElement(By.id("genButton")).click();
-        // Wait for the word to appear
-        if (wait.until(ExpectedConditions.textToBePresentInElementLocated(By.id("word"), "release"))) {
-            // Print the text to console
-            System.out.println("Word found: " + driver.findElement(By.id("word")).getText());
-        }
+        // Find the checkbox
+        WebElement checkbox = driver.findElement(By.id("checkbox"));
+        // Click it
+        checkbox.click();
+        // Check if it is selected
+        System.out.println("Checkbox is selected: " + checkbox.isSelected());
+        // Click the checkbox again
+        checkbox.click();
+        // Check if it is selected
+        System.out.println("Checkbox is selected: " + checkbox.isSelected());
+
         // Close the browser
         driver.quit();
     }
