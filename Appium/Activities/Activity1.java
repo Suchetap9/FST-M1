d801c8a6b8ec4c8b8bd087c2e82977d8d91d4837
Suchetap9 <suchepat@in.ibm.com>
2025-04-04 16:57:20 +0530

    Update Activity1.java

--- Appium/Activities/Activity1.java
+++ Appium/Activities/Activity1.java
@@ -1,64 +1,66 @@
 package activities;
 
+import java.net.URL;
+
+import static org.testng.Assert.assertEquals;
 
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
 
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.AppiumBy;
+import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 
+
 public class Activity1 {
-    // Driver Declaration
-    AndroidDriver driver;
-
-    // Set up method
-    @BeforeClass
-    public void setUp() throws MalformedURLException, URISyntaxException {
-        // Desired Capabilities
-        UiAutomator2Options options = new UiAutomator2Options();
-        options.setPlatformName("android");
-        options.setAutomationName("UiAutomator2");
-        options.setAppPackage("com.google.android.calculator");
-        options.setAppActivity("com.android.calculator2.Calculator");
-        options.noReset();
-
-        // Server Address
-        URL serverURL = new URI("http://127.0.0.1:4723").toURL();
-
-        // Driver Initialization
-        driver = new AndroidDriver(serverURL, options);
-    }
-
-    // Test method
-    @Test
-    public void calculatorTest() {
-        // Perform the calculation
-        driver.findElement(AppiumBy.id("digit_5")).click();
-        driver.findElement(AppiumBy.accessibilityId("multiply")).click();
-        driver.findElement(AppiumBy.id("digit_6")).click();
-        driver.findElement(AppiumBy.accessibilityId("equals")).click();
-
-        // Find the result
-        String result = driver.findElement(AppiumBy.id("result_final")).getText();
-
-        // Assertion
-        Assert.assertEquals(result, "30");
-    }
-
-
-    // Tear down method
-    @AfterClass
-    public void tearDown() {
-        // Close the app
-        driver.quit();
-    }
+	
+	AppiumDriver driver;
+	
+	@BeforeClass
+	public void andriodSetup() throws MalformedURLException, URISyntaxException {
+		//set desired capabilities
+		UiAutomator2Options options = new UiAutomator2Options()
+				.setPlatformName("andriod")//Mandatory
+				.setAutomationName("UiAutomator2")//Mandatory
+				.setAppPackage("com.miui.calculator")//to set a package, its always goes with setAppActivity()
+				.setAppActivity(".cal.CalculatorActivity")//to set a class, its always goes with setAppPackage()
+				.noReset(); //it will not reset the appium
+				
+		//set appium url
+		URL serverURL = new URI("http://127.0.0.1:4723").toURL();
+				
+		
+		//Initialize driver
+		driver = new AndroidDriver(serverURL, options);
+		
+	}
+	
+	@Test
+	public void variousLocators() {
+		//using accesibilty id,
+		driver.findElement(AppiumBy.accessibilityId("clear")).click();
+		
+		driver.findElement(AppiumBy.id("com.miui.calculator:id/btn_8_s")).click();
+		
+		driver.findElement(AppiumBy.accessibilityId("multiply")).click();
+		
+		driver.findElement(AppiumBy.id("com.miui.calculator:id/btn_3_s")).click();
+		
+		driver.findElement(AppiumBy.accessibilityId("equals")).click();
+		
+		String res = driver.findElement(AppiumBy.xpath("//android.widget.ViewText[@resource-id='result")).getText();
+		assertEquals(res, "= 24");
+		
+	}
+	
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
 }
-
